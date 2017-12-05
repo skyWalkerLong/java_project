84ecceac6972ff72a2d4c8fdd114060a8a2865e0
longchao <longchao@jd.com>
2017-12-05 10:24:00 +0800

    1

--- src/main/java/arraylist/InitSize.java
+++ src/main/java/arraylist/InitSize.java
@@ -8,12 +8,14 @@ import java.util.List;
  * @Date 2017/11/30.
  */
 public class InitSize {
+    //类的属性自动初试化为0，如果是方法中定义的变量则必须初始化
     private static int a;
     public static void main(String[] args) {
         List<Integer> appId = new ArrayList<>();
         System.out.println(a);
         appId.add(1);
         appId.add(2);
+        //初始化容量为2，只是初始化数组长度为2，集合的长度依然为0，看源码
         List<Integer> result = new ArrayList<>(2);
         System.out.println(result.size());
 //        result.set(1,2);
