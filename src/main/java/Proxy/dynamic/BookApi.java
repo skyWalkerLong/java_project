434f73c25ec2062367484d89095bb81a2d685881
longchao <longchao@jd.com>
2018-09-10 10:27:48 +0800

    proxy

---
+++ src/main/java/Proxy/dynamic/BookApi.java
@@ -0,0 +1,9 @@
+package Proxy.dynamic;
+
+/**
+ * @author longchao
+ * @date 2018/8/9.
+ */
+public interface BookApi {
+    void sell();
+}
---
+++ src/main/java/Proxy/dynamic/BookApiImpl.java
@@ -0,0 +1,10 @@
+package Proxy.dynamic;
+
+/**
+ * @author longchao
+ * @date 2018/8/9.
+ */
+public class BookApiImpl implements BookApi {
+    @Override
+    public void sell(){}
+}
